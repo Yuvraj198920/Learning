76299cd6c082b4438a48a38b1ff46558d173ad34
yuvraj <yuvvraaj0flying@gmail.com>
2023-08-10 13:27:32 +0800

    Added appropriet comments

--- Data_structure/binary_search.py
+++ Data_structure/binary_search.py
@@ -9,12 +9,17 @@ steps = 0
 def binary_search (arr, low, high, target):
     if (high >= low):
         global steps
+        # to check how many recurrsion it took to find the target element
         steps += 1
+        # Find mid
         mid  = calculate_mid(low, high)
+        # if mid is the target element
         if arr[mid] == target:
             return mid
+        # if the target elemet is lesser than mid then it is in the right portion of the array
         elif arr[mid] > target:
             return binary_search(arr, low, mid-1, target)
+        # if the target elemet is greater than mid then it is in the left portion of the array
         else:
             return binary_search(arr, mid+1, high, target)
     else:
