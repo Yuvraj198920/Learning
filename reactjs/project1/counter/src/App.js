f7f42d3a6c537590529e3b27e108093dde2b7107
yuvraj <yuvvraaj0flying@gmail.com>
2023-08-23 21:10:22 +0800

    fetch and display data

--- reactjs/project1/counter/src/App.js
+++ reactjs/project1/counter/src/App.js
@@ -1,19 +1,44 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // https://randomuser.me/api
 function App() {
   const [counter, setCounter] = useState(0);
+  const [data, setData] = useState(null)
+
+  const fetchData = () => {
+    fetch("https://randomuser.me/api").then((res) => {
+      return res.json()
+    }).then(data => {
+      const stringFy = JSON.stringify(data)
+      console.log(data)
+      setData(stringFy)
+    })
+  }
+
+  // useEffect(() => {
+  //   fetchData()
+  // }, []);
 
   return (
     <div className="App">
       <h1>Counter App</h1>
       <p>{counter}</p>
       <button onClick={() => {
-        setCounter(counter+1)
+        setCounter(counter + 1)
       }}>Increment</button>
       <button onClick={() => {
-        setCounter(counter-1)
+        setCounter(counter - 1)
       }}>Decrement</button>
+
+      <button onClick={() => {
+        fetchData()
+      }}>FetchData</button>
+
+      <div>
+        <pre>
+          {data}
+        </pre>
+      </div>
     </div>
   );
 }
