3ef5f20819023d19c44ae9e376d1352f4333e6b0
yuvraj <yuvvraaj0flying@gmail.com>
2023-08-24 12:06:58 +0800

    Added comments

--- Algorithms/selection_sort.py
+++ Algorithms/selection_sort.py
@@ -2,11 +2,13 @@ arr = [3,1,9,5,6,7]
 
 def selection_sort(arr):
     for i in range(len(arr)):
+        # hold the index of minimum element
         min_index = i
         for j in range(i+1, len(arr)):
+            # check if element at min_index is greater than next element
             if arr[min_index] > arr[j]:
                 min_index = j
-            
+        # swap the elememnts
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
