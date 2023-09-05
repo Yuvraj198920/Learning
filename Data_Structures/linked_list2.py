bc70c3510a1b7f0e79388ca30bb3387033badf37
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-09-05 20:46:36 +0530

    append functionality for linked list

--- Data_Structures/linked_list2.py
+++ Data_Structures/linked_list2.py
@@ -5,4 +5,14 @@ class Node:
 
 class LnkedList:
     def __init__(self):
-        self.head = None
+        self.head = Node()
+
+    def append(self, data):
+        new_node = Node(data)
+        cur = self.head
+        # Loop through linked list to reach at the end
+        while cur.next != None:
+            cur = cur.next
+
+            # Now you are at the end of list append your new node
+        cur.next = new_node
