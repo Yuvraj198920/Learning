e32ef12bb261be91e3ffe2f1b733f8d145e1ff7f
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-09-14 20:43:05 +0530

    Check if table exists

--- gis/check_db.py
+++ gis/check_db.py
@@ -9,3 +9,12 @@ fc_list = arcpy.ListFeatureClasses()
 
 for fc in fc_list:
     print(fc)
+
+# table
+table_name = "tablename"
+
+# Check if the table exists in sde
+if arcpy.Exists(table_name):
+    print(f"{table_name} is present in sde")
+else:
+    print(f"{table_name} is not present inside sde")
